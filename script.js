3d7eba069145bf91dabc7c4d7c0526e761b8bf5d
manul <manuuva5@gmail.com>
2025-03-05 23:43:07 -0300

    final

---
+++ script.js
@@ -0,0 +1,44 @@
+function makeGrid(size=16) {
+    let grid = document.querySelector('.grid-container')
+    for (let i = 0; i < size; i++) {
+        let row = document.createElement('div')
+        row.setAttribute('class', 'row')
+        grid.appendChild(row)
+        for(let j = 0; j < size; j++){
+            let gridElement = document.createElement('div')
+            gridElement.setAttribute('class', 'grid-element')
+            gridElement.addEventListener('mouseover', (e) => {
+                gridElement.setAttribute('style', 'background-color: black')
+            })
+            row.appendChild(gridElement)
+        }
+    }
+}
+
+function cleanGrid() {
+    let grid = document.querySelector('.grid-container')
+    grid.innerHTML = ''
+}
+
+let size = 16
+
+const btn = document.querySelector('.button')
+const btn2 = document.querySelector('.button-2')
+
+btn2.addEventListener('click', () => {
+    cleanGrid()
+    makeGrid(size)
+})
+
+btn.addEventListener('click', () => {
+    let temp = prompt('Input the size of the new grid. Less than 100.')
+    if (temp > 100) {
+        alert('The size cant be larger than 100')
+    } else {
+        size = temp
+        cleanGrid()
+        makeGrid(size)
+    }
+})
+
+makeGrid(size)
